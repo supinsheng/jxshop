dee43a95a21a7e21d32f6e667577921de43e27d2
supinsheng <1390499468@qq.com>
2018-09-25 09:56:24 +0800

    提交 后台模板

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -5,7 +5,18 @@ namespace controllers;
 class IndexController {
 
     public function index(){
-
         view('index.index');
+    }
+
+    public function top(){
+        view('index.top');
+    }
+
+    public function main(){
+        view('index.main');
+    }
+
+    public function menu(){
+        view('index.menu');
     }
 }
